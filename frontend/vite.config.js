5e9b4a572f31502455769e6f71ed7ea02fb74be1
steven-mrsdigi <steven.mrsdigi@gmail.com>
2026-02-15 16:33:17 -0500

    front end

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,6 +9,10 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
   server: {
     port: 3000,
     proxy: {
