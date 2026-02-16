49c428e345335e809fe30d8d7173561a524c8937
steven-mrsdigi <steven.mrsdigi@gmail.com>
2026-02-16 17:07:22 -0500

    Add PATCH support for updating customers

--- backend/api_customers.php
+++ backend/api_customers.php
@@ -28,6 +28,25 @@ function handleCustomers($method, $input) {
             echo json_encode(['message' => 'Customer created', 'data' => $result['data']]);
             break;
             
+        case 'PATCH':
+            if (!isset($input['id'])) {
+                http_response_code(400);
+                echo json_encode(['error' => 'Customer ID required']);
+                return;
+            }
+            
+            $updateData = [];
+            if (isset($input['name'])) $updateData['name'] = $input['name'];
+            if (isset($input['email'])) $updateData['email'] = $input['email'];
+            if (isset($input['phone'])) $updateData['phone'] = $input['phone'];
+            if (isset($input['address'])) $updateData['address'] = $input['address'];
+            if (isset($input['company'])) $updateData['company'] = $input['company'];
+            if (isset($input['status'])) $updateData['status'] = $input['status'];
+            
+            $result = supabaseRequest('customers', 'PATCH', $updateData, 'id=eq.' . $input['id']);
+            echo json_encode(['message' => 'Customer updated', 'data' => $result['data']]);
+            break;
+            
         default:
             http_response_code(405);
             echo json_encode(['error' => 'Method not allowed']);
